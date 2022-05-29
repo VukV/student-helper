90adba128fc7f21e099eeb0135282d0068e646f3
VukV <vuk.vukovic.2000@gmail.com>
2022-05-29 17:26:27 +0200

    working on edit note fragment

--- app/src/main/java/rs/raf/projekat2/vuk_vukovic_rn9420/presentation/view/fragments/EditNoteFragment.kt
+++ app/src/main/java/rs/raf/projekat2/vuk_vukovic_rn9420/presentation/view/fragments/EditNoteFragment.kt
@@ -5,12 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import rs.raf.projekat2.vuk_vukovic_rn9420.R
 import rs.raf.projekat2.vuk_vukovic_rn9420.databinding.FragmentEditNoteBinding
+import rs.raf.projekat2.vuk_vukovic_rn9420.presentation.contract.NoteContract
+import rs.raf.projekat2.vuk_vukovic_rn9420.presentation.viewmodel.NoteViewModel
 
-class EditNoteFragment : Fragment(R.layout.fragment_edit_note) {
+class EditNoteFragment(
+    private val noteId: Int,
+    private val noteTile: String,
+    private val noteContent: String,
+    private val noteArchived: Boolean
+) : Fragment(R.layout.fragment_edit_note) {
 
-    //TODO konstruktor prima Note koji se menja
+    private val noteViewModel: NoteContract.ViewModel by sharedViewModel<NoteViewModel>()
 
     private var _binding: FragmentEditNoteBinding? = null
     private val binding get() = _binding!!
@@ -29,14 +38,37 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note) {
 
         initNote()
         initListeners()
-    }
-
-    private fun initListeners(){
-        //todo
+        initObservers()
     }
 
     private fun initNote(){
-        //todo
+        binding.editTitle.setText(noteTile)
+        binding.editContent.setText(noteContent)
+    }
+
+    private fun initListeners(){
+        binding.cancelEditButton.setOnClickListener {
+            noteViewModel.setNeutral()
+            activity?.onBackPressed()
+        }
+
+        binding.saveEditButton.setOnClickListener {
+            val newTitle = binding.editTitle.text.toString()
+            val newContent = binding.editContent.text.toString()
+
+            if (newTitle != "" && newContent != ""){
+                noteViewModel.update(noteId, newTitle, newContent, noteArchived)
+            }
+            else{
+                //TODO toast
+            }
+        }
+    }
+
+    private fun initObservers(){
+        noteViewModel.editNoteState.observe(viewLifecycleOwner, Observer {
+            //TODO
+        })
     }
 
     override fun onDestroyView() {
